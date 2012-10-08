569ed74fea7ccecef191e54e6164be2738a4ba12
John Nolan <john.nolan@moneyplusgroup.co.uk>
2012-10-08 16:56:42 +0100

    changed from gross price to net + tax

--- SalesTax/Receipt.cs
+++ SalesTax/Receipt.cs
@@ -12,9 +12,9 @@ namespace SalesTax
             foreach (var basketItem in basket)
             {
                 sb.Append(string.Format("{0} {1}: {2:0.00} ", basketItem.Quantity,
-                                            basketItem.Item.ProductName.ToLower(), basketItem.Item.GrossPrice));
+                                            basketItem.Item.ProductName.ToLower(), basketItem.Item.NetPrice+basketItem.Item.Tax));
             }
-            sb.Append(string.Format("Sales Taxes: {0:0.00} Total: {1:0.00}",basket.Sum(x => x.Item.Tax), basket.Sum(x => x.Item.GrossPrice)));
+            sb.Append(string.Format("Sales Taxes: {0:0.00} Total: {1:0.00}",basket.Sum(x => x.Item.Tax), basket.Sum(x => x.Item.NetPrice+x.Item.Tax)));
             return sb.ToString();
         }
     }
--- SalesTax/TaxableItem.cs
+++ SalesTax/TaxableItem.cs
@@ -3,14 +3,14 @@ namespace SalesTax
     public class TaxableItem : IItem
     {
         public string ProductName { get; set; }
-        public decimal GrossPrice { get; set; }
+        public decimal NetPrice { get; set; }
         public decimal Tax { get; set; }
 
-        internal TaxableItem(string productName, decimal grossPrice)
+        internal TaxableItem(string productName, decimal netPrice)
         {
             ProductName = productName;
-            GrossPrice = grossPrice * 1.1m;
-            Tax = grossPrice * 0.1m; //TODO: magic number
+            NetPrice = netPrice;
+            Tax = netPrice * 0.1m; //TODO: magic number
         }
     }
 }
